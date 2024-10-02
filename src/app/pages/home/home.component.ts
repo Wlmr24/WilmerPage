333e8fd3aba2fe6fa1a754e9af5f851ae125e468
wlmr24 <wilmergr24@gmail.com>
2024-10-01 19:23:24 -0500

    Nueva hoja de vida

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,13 +8,13 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
       // URL del archivo PDF que deseas descargar
-      private cvUrl = 'assets/archivos/CV-Wilmer 2024.pdf'; // Cambia esta URL a la ruta de tu archivo PDF
+      private cvUrl = 'assets/archivos/CV-Wilmer-2024.pdf'; // Cambia esta URL a la ruta de tu archivo PDF
 
       // Función para manejar la descarga del archivo PDF
       downloadCV() {
         const link = document.createElement('a');
         link.href = this.cvUrl;
-        link.download = 'CV-Wilmer 2024.pdf'; // Nombre del archivo que se descargará
+        link.download = 'CV-Wilmer-2024.pdf'; // Nombre del archivo que se descargará
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
